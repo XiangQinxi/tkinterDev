899f5f067f9e7c77421a00abfccc7cf61122bf13
Administrator <XiangQinxi@outlook.com>
2022-07-05 11:53:34 +0800

    1.6.0

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="tkinterDev",
+    version="1.6.0",
+    author="XiangQinxi",
+    author_email="XiangQinxi@outlook.com",
+    description="tkinter Tool",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
